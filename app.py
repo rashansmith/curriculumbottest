006e24f5f954cc5b849816cce46b90a5a384e58f
erashan2 <erashan2@illinois.edu>
2017-07-26 14:06:23 -0400

    init

---
+++ app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "exam.worth":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    type = parameters.get("exam-type")
+
+    worth = {'Mid Term 1':20, 'Mid Term 2':20, 'Final Exam':50, 'Extra Credit Exam':10}
+
+    speech = "The" + worth + " is worth " + str(exams[worth]) + " Percent of your grade."
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "syllabus"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
